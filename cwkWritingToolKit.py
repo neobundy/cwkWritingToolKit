5931307c51a4da250a8a64a9a9748c4a0c942df5
Wankyu Choi <wankyuchoi@gmail.com>
2014-05-12 22:21:20 +0900

    Fixes bug: active view never changes

--- cwkWritingToolKit.py
+++ cwkWritingToolKit.py
@@ -14,22 +14,25 @@ class CwkAutoComplete(sublime_plugin.WindowCommand):
 		self.plugin_settings = sublime.load_settings("cwkWritingToolKit.sublime-settings")
 		super().__init__(window)
 		self._words = ["foo", "bar"]
-		self.window = sublime.active_window()
-		self.view = window.active_view()
 
 	def run(self):
 		self.debug = self.plugin_settings.get("debug", False)
-		self.view.show_popup_menu(self._words, self.on_done)
+		window = sublime.active_window()
+		view = window.active_view()
+		self.log("view id: ", view.view_id)
+
+		view.show_popup_menu(self._words, self.on_done)
 
 	def on_done(self, index):
 		if index == -1: return
 		selected_string = self._words[index]
 		self.log("Word selected: " , selected_string)
-		self.view.run_command("cwk_insert_selected_text", {"args": {'text': selected_string} })
+		window = sublime.active_window()
+		view = window.active_view()
+		view.run_command("cwk_insert_selected_text", {"args": {'text': selected_string} })
 
 	def log(self, *message):
 		if(self.debug):
 			print (*message)
 
 
-			
