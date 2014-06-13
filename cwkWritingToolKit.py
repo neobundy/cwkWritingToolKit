034f3c487ef80951c868b8751ea63d32bf7875dc
Wankyu Choi <wankyuchoi@gmail.com>
2014-06-14 02:11:11 +0900

    Bug fix: Should catch AttributeError

--- cwkWritingToolKit.py
+++ cwkWritingToolKit.py
@@ -82,7 +82,7 @@ class cwkBase:
 		try:
 			fname, fextension = os.path.splitext(filename)
 			return fextension in self.corpus_extensions
-		except TypeError as e:
+		except AttributeError as e:
 			self.log("Error reading {filename}: {error}".format(filename=filename, error=e))
 			return False
 
@@ -93,7 +93,7 @@ class cwkBase:
 		try:
 			fname, fextension = os.path.splitext(filename)
 			return fextension in self.custom_dictionary_extensions
-		except TypeError as e:
+		except AttributeError as e:
 			self.log("Error reading {filename}: {error}".format(filename=filename, error=e))
 			return False
 
